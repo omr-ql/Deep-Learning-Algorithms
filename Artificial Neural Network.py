2952f2829eadae40eec174f6a7f69c7afb6fba4d
OMR ABDULLAH <121067316+omr-ql@users.noreply.github.com>
2025-01-25 20:02:57 +0200

    Artificial Neural Network.py

--- Artificial Neural Network.py
+++ Artificial Neural Network.py
@@ -9,7 +9,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 # Part 1 - Data Preprocessing
 # Importing the dataset
@@ -24,8 +24,8 @@ print(y)
 le = LabelEncoder()
 X[:, 2] = le.fit_transform(X[:, 2])
 print(X)
-# One Hot Encoding the "Geography" column
 
+# One Hot Encoding the "Geography" column
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 print(X)
@@ -39,16 +39,17 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Part 2 - Building the ANN
-
 # Initializing the ANN
 ann = tf.keras.models.Sequential()
 
 # Adding the input layer and the first hidden layer
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 
-# Adding the second hidden layer
+# Adding more hidden layers 
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-
+ann.add(tf.keras.layers.Dense(units=6, activation='sigmoid'))
+ann.add(tf.keras.layers.Dense(units=6, activation='sigmoid'))
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 # Adding the output layer
 ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
@@ -58,7 +59,7 @@ ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Training the ANN on the Training set
-ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+ann.fit(X_train, y_train, batch_size = 64, epochs = 50)
 
 # Part 4 - Making the predictions and evaluating the model
 
@@ -71,7 +72,6 @@ y_pred = (y_pred > 0.5)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
